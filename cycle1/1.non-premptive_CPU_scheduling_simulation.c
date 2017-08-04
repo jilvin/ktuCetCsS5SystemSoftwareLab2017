57a38385f75876b4db610f039e48a9b43d25c25f
Jilvin <jilvinjacob@gmail.com>
2017-08-04 08:18:29 +0530

    working on 1.1

---
+++ cycle1/1.non-premptive_CPU_scheduling_simulation.c
@@ -0,0 +1,12 @@
+// 1) Simulate the following non-preemptive CPU scheduling algorithms to find turnaround time and waiting time.
+// a) FCFS b) SJF c) Round Robin (pre-emptive) d) Priority
+
+#include <stdio.h>
+
+int main()
+{
+  int choice;
+  printf("Non-preemptive CPU Scheduling Simulation\n\n1)\tFCFS Scheduling Algorithm Simulation\n2)\tSJF Scheduling Algorithm Simulation\n3)\tRound Robin Scheduling Algorithm Simulation\n4)\tPriority Scheduling Algorithm Simulation\n\nPlease enter your choice:\n");
+  choice = getchar();
+  return 1;
+}
